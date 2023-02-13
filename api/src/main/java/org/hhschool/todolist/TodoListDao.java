ed9d95b92d3b0861bf9994b350976bdec980f8f4
evgeny <polevikovea@gmail.com>
2023-02-13 16:44:14 +0300

    Remove unnecessary comments

--- api/src/main/java/org/hhschool/todolist/TodoListDao.java
+++ api/src/main/java/org/hhschool/todolist/TodoListDao.java
@@ -7,26 +7,19 @@ import java.util.*;
 
 @Component
 public class TodoListDao {
-  //private final List<TodoItem> todos = Collections.synchronizedList(new LinkedList<>());
   @Autowired
   private TodoItemRepository todoItemRepository;
 
   public Iterable<TodoItem> all() {
     return todoItemRepository.findAll();
-    //return todos;
   }
 
   public Optional<TodoItem> get(Long id) {
     return todoItemRepository.findById(id);
-    /*
-    return todos.stream()
-      .filter(item -> Objects.equals(item.getId(), id))
-      .findFirst(); */
   }
 
   public TodoItem save(TodoItem item) {
     return todoItemRepository.save(item);
-    //todos.add(item);
   }
 
   public TodoItem update(TodoItem newItem) {
@@ -39,7 +32,6 @@ public class TodoListDao {
 
   public void delete(Long id) {
     todoItemRepository.deleteById(id);
-    //todos.removeIf(item -> Objects.equals(item.getId(), id));
   }
 
   public void deleteAll() {
